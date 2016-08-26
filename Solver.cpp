36dbbed19f10d27bb70b8c70bab4c28f057d673c
Hitoshi Togasaki <togasakitogatoga@gmail.com>
2016-08-26 19:02:36 +0900

    Implement Lit(Literal)

---
+++ Solver.cpp
@@ -0,0 +1,48 @@
+// SAT Solver
+// CDCL Solver
+namespace Togasat {
+
+class Solver {
+
+public:
+  using Var = int;
+  const int var_Undef = -1;
+
+  // Literal
+  struct Lit {
+    int x;
+    inline bool operator==(Lit p) const { return x == p.x; }
+    inline bool operator!=(Lit p) const { return x != p.x; }
+    inline bool operator<(Lit p) const { return x < p.x; }
+
+    inline Lit operator~() {
+      Lit q;
+      q.x = x ^ 1;
+      return q;
+    }
+  };
+
+  inline Lit mkLit(Var var, bool sign) {
+    Lit p;
+    p.x = var + var + sign;
+    return p;
+  };
+
+  inline bool sign(Lit p) { return p.x & 1; }
+  inline int var(Lit p) { return p.x >> 1; }
+
+  inline int toInt(Var v) { return v; }
+  inline int toInt(Lit p) { return p.x; }
+  inline Lit toLit(int x) {
+    Lit p;
+    p.x = x;
+    return p;
+  }
+  const Lit lit_Undef = {-2};
+  const Lit lit_Error = {-1};
+
+  
+};
+}
+
+int main() {}
