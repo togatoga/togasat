c5a5682e9cab45905719e01891a75a91b452ae32
togatoga <togasakitogatoga@gmail.com>
2019-08-16 15:40:49 +0900

    WIP

--- example/sudoku/main.cpp
+++ example/sudoku/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-#include "togasat.hpp"
-
-
+#include <cassert>
+#include "../../togasat.hpp"
+int height,width;
+int calculate_index(int y, int x, int value)
+{
+    assert(value >= 1 && value <= 9);
+    int index = 9 * 9 * y + 9 * x + value;
+    return index;
+}
 int main() {
-
+    togasat::Solver solver;
+    std::cin >> height >> width;
+    for (size_t i = 0; i < height; i++)
+    {
+        for (size_t j = 0; j < width; j++)
+        {
+            std::string text;
+            std::cin >> text;
+        }
+    }
 }
